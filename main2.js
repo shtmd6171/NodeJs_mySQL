e15f6cfee669e8e8da3bc5058a265ca9b6bd0885
shtmd6171 <samantha.n,morton@gmail.com>
2020-09-02 14:50:29 +0900

    수정 삭제

--- main2.js
+++ main2.js
@@ -3,9 +3,6 @@
 mysql 쿼리문을 통해서 처리하게 변경한다.
 */
 
-
-
-
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
@@ -126,28 +123,22 @@ var app = http.createServer(function(request,response){
           });
       });
     } else if(pathname === '/update'){
-      fs.readdir('./data', function(error, filelist){
-        var filteredId = path.parse(queryData.id).base;
-        fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-          var title = queryData.id;
-          var list = template.list(filelist);
-          var html = template.HTML(title, list,
-            `
-            <form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${title}">
-              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-              <p>
-                <textarea name="description" placeholder="description">${description}</textarea>
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-            `,
-            `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-          );
-          response.writeHead(200);
-          response.end(html);
+      db.query('SELECT * FROM topic',function(error,topics){
+        if(error) throw error
+        db.query('SELECT * FROM topic WHERE id=?',[queryData.id],function(error2,topic){
+          if(error2) throw error2
+           var title = 'Web - update';
+           var list = template.list(topics);
+           var html = template.HTML(title, list,
+            `<form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${topic[0].id}">
+                <p><input type="text" name="title" placeholder="title" value="${topic[0].title}"></p>
+                <p><textarea name="description" placeholder="description">${topic[0].description}</textarea></p>
+                <p><input type="submit"></p></form>`,
+                `<a href="/create">create</a> <a href="/update?id=${topic[0].id}">update</a>`
+               );
+               response.writeHead(200);
+               response.end(html);
         });
       });
     } else if(pathname === '/update_process'){
@@ -160,11 +151,16 @@ var app = http.createServer(function(request,response){
           var id = post.id;
           var title = post.title;
           var description = post.description;
-          fs.rename(`data/${id}`, `data/${title}`, function(error){
-            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-              response.writeHead(302, {Location: `/?id=${title}`});
-              response.end();
-            })
+          // fs.rename(`data/${id}`, `data/${title}`, function(error){
+          //   fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+          //     response.writeHead(302, {Location: `/?id=${title}`});
+          //     response.end();
+          //   })
+          // });
+          db.query("UPDATE topic SET title=?, description=? WHERE id = ? author_id = ?", [post.title,post.description,post.id,1],function(error,topic){
+            if(error) throw error;
+            response.writeHead(302, {Location: `/?id=${post.id}`});
+            response.end();
           });
       });
     } else if(pathname === '/delete_process'){
@@ -174,13 +170,12 @@ var app = http.createServer(function(request,response){
       });
       request.on('end', function(){
           var post = qs.parse(body);
-          var id = post.id;
-          var filteredId = path.parse(id).base;
-          fs.unlink(`data/${filteredId}`, function(error){
-            response.writeHead(302, {Location: `/`});
+          db.query("DELETE FROM topic WHERE id = ?",[post.id],function (error,topic) {
+            if(error) throw error
+            response.writeHead(302,{Location: `/`});
             response.end();
-          })
-      });
+          });
+        });
     } else {
       response.writeHead(404);
       response.end('Not found');
